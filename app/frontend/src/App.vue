521884bfffa87e58472c1355d3b12439b9fb06cb
Chelong <chelong821204@gmail.com>
2022-06-13 17:23:49 +0800

    practice: add component, props, computed

--- app/frontend/src/App.vue
+++ app/frontend/src/App.vue
@@ -1,15 +1,15 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <Foot author="chelong" />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Foot from './components/Foot.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Foot
   }
 }
 </script>
---
+++ app/frontend/src/components/Foot.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="footer">
+        <div class="footer-content"> {{ trademark }}</div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'FootBlock',
+  props: ['author'],
+  data: function () {
+    return {
+      trademarkPrefix: 'created by'
+    }
+  },
+  computed: {
+    trademark () {
+      return this.trademarkPrefix + ' ' + this.author + ' !'
+    }
+  }
+}
+</script>
+<style scoped>
+  .footer{
+    background-color: rgb(252, 252, 218);
+    width: 100%;
+    height: 20vh;
+    position:absolute;
+    bottom:0px;
+    left:0px;
+    font-size: 10px;
+  }
+  .footer-content{
+    padding-top: 60px;
+    text-align: center;
+  }
+</style>
