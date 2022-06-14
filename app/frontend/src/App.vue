674b2cde7c8e3b01db74bf32bee0fbef73950d46
Chelong <chelong821204@gmail.com>
2022-06-14 16:14:42 +0800

    practice: using v-bind to pass value, using v-model to dynamic maintain data

--- app/frontend/src/App.vue
+++ app/frontend/src/App.vue
@@ -1,6 +1,19 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <Foot author="chelong" />
+  <div class="content-form">
+    <div class="field is-horizontal">
+      <div class="field-body">
+        <div class="field">
+          <p class="control">
+            <input class="input" type="text" placeholder="你的暱稱" v-bind:nickname="name" v-model="name">
+          </p>
+        </div>
+      </div>
+    </div>
+    <button class="button is-primary is-light" v-on:click="thankyou">送出</button>
+  </div>
+  <Foot v-bind:author="name" />
+
 </template>
 
 <script>
@@ -8,6 +21,16 @@ import Foot from './components/Foot.vue'
 
 export default {
   name: 'App',
+  data: function () {
+    return {
+      name: ''
+    }
+  },
+  methods: {
+    thankyou () {
+      alert(`${this.name}, alert success!`)
+    }
+  },
   components: {
     Foot
   }
