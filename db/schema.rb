9cac64cdde1e648c3d7ba057ab5b669ff4b09ef5
Chelong <chelong821204@gmail.com>
2022-06-22 13:08:56 +0800

    create board

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_19_145753) do
+ActiveRecord::Schema.define(version: 2022_06_21_081012) do
+
+  create_table "boards", force: :cascade do |t|
+    t.string "name"
+    t.string "description"
+    t.integer "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_boards_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "provider", default: "email", null: false
@@ -37,4 +46,5 @@ ActiveRecord::Schema.define(version: 2022_06_19_145753) do
     t.index ["uid", "provider"], name: "index_users_on_uid_and_provider", unique: true
   end
 
+  add_foreign_key "boards", "users"
 end
