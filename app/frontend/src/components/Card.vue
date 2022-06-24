aef700c4bdc9aee3a6338f6651eff731f0a212fe
Chelong <chelong821204@gmail.com>
2022-06-24 18:38:11 +0800

    add card component

---
+++ app/frontend/src/components/Card.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="my-3 px-6 py-2 border-2 border-gray-200"> {{ card.name }}</div>
+</template>
+
+<script>
+export default {
+  name: 'CardBlock',
+  props: ['card']
+}
+</script>
--- app/frontend/src/views/BoardView.vue
+++ app/frontend/src/views/BoardView.vue
@@ -15,6 +15,7 @@
         <p class="text-gray-700 text-base mb-4">
           {{ list.description }}
         </p>
+        <Card v-for="card in list.cards" :key="card.id" :card="card" />
         <a :href="'/lists/'+list.id+'/lists'" type="button"
           class=" inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out">Create New Card</a>
       </div>
@@ -23,6 +24,7 @@
 </template>
 
 <script>
+import Card from '../components/Card.vue'
 import { mapGetters } from 'vuex'
 import axios from 'axios'
 const apiUrlPrefix = 'http://localhost:3000/api/v1/boards/'
@@ -35,6 +37,9 @@ export default {
       name: '',
       apiUrl: ''
     }
+  },
+  components: {
+    Card
   },
   computed: {
     ...mapGetters(['isLogin']),
