b7bb2fcb409f013fb32b01cc8cd8d982e2d2d5f3
Chelong <chelong821204@gmail.com>
2022-06-25 00:25:17 +0800

    use draggable plugin to accomplish frontend draggable effect

--- app/frontend/src/views/BoardView.vue
+++ app/frontend/src/views/BoardView.vue
@@ -1,30 +1,34 @@
 <template>
-  <div class="p-10 grid grid-cols-1 sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-3 gap-5">
-    <div class="block p-6 rounded-lg shadow-lg bg-white max-w-sm">
-      <form @submit="onSubmit">
-        <h5 class="text-gray-900 text-xl leading-tight font-medium mb-2">Add List</h5>
-        <input type="text" class="text-gray-700 text-base mb-4" v-model="name" placeholder="type list name ... " />
-        <input type="submit" value="Create New List"
-          class=" inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"/>
-      </form>
-    </div>
-    <div v-for="list in lists" :key="list.id">
+  <draggable v-model="lists" item-key="id"
+    class="p-10 grid grid-cols-1 sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6 gap-5">
+
+    <template #item="{element}">
       <div class="block p-6 rounded-lg shadow-lg bg-white max-w-sm">
-        <i @click="deleteList(list.id)" class="flex flex-row-reverse">X</i>
-        <h5 class="text-gray-900 text-xl leading-tight font-medium mb-2">{{ list.name }}</h5>
+        <i @click="deleteList(element.id)" class="flex flex-row-reverse">X</i>
+        <h5 class="text-gray-900 text-xl leading-tight font-medium mb-2">{{ element.name }}</h5>
         <p class="text-gray-700 text-base mb-4">
-          {{ list.description }}
+          {{ element.description }}
         </p>
-        <Card v-for="card in list.cards" :key="card.id" :card="card" />
-        <a :href="'/lists/'+list.id+'/lists'" type="button"
-          class=" inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out">Create New Card</a>
+        <Card v-for="card in element.cards" :key="card.id" :card="card" />
+        <a :href="'/lists/'+element.id+'/lists'" type="button"
+          class=" inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out">Create
+          New Card</a>
       </div>
-    </div>
+    </template>
+  </draggable>
+  <div class="block m-10 p-6 rounded-lg shadow-lg bg-white max-w-sm">
+    <form @submit="onSubmit">
+      <h5 class="text-gray-900 text-xl leading-tight font-medium mb-2">Add List</h5>
+      <input type="text" class="text-gray-700 text-base mb-4" v-model="name" placeholder="type list name ... " />
+      <input type="submit" value="Create New List"
+        class=" inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out" />
+    </form>
   </div>
 </template>
 
 <script>
 import Card from '../components/Card.vue'
+import draggable from 'vuedraggable'
 import { mapGetters } from 'vuex'
 import axios from 'axios'
 const apiUrlPrefix = 'http://localhost:3000/api/v1/boards/'
@@ -39,7 +43,8 @@ export default {
     }
   },
   components: {
-    Card
+    Card,
+    draggable
   },
   computed: {
     ...mapGetters(['isLogin']),
