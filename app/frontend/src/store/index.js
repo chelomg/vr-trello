241ecc049d90e57bf529e16d35a42ea88cbded3e
Chelong <chelong821204@gmail.com>
2022-07-06 18:13:28 +0800

    remove practicing code

--- app/frontend/src/store/index.js
+++ app/frontend/src/store/index.js
@@ -1,11 +1,9 @@
 import { createStore } from 'vuex'
-import counter from './modules/counter.js'
 import login from './modules/login.js'
 import list from './modules/list.js'
 
 export default createStore({
   modules: {
-    counter,
     login,
     list
   }
