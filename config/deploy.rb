818c2b6e2f1cc25f955d6c042389104d6f0f6f1d
Chelong <chelong821204@gmail.com>
2022-08-02 13:03:21 +0800

    try to fix key form config file

--- config/deploy.rb
+++ config/deploy.rb
@@ -23,7 +23,7 @@ set :branch, "main"
 
 # Default value for :linked_files is []
 # append :linked_files, "config/database.yml", 'config/master.key'
-append :linked_files#, 'config/secrets.yml'
+append :linked_files, 'config/secrets.yml'
 
 # Default value for linked_dirs is []
 # append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "tmp/webpacker", "public/system", "vendor", "storage"
