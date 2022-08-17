f1de410127f0b9a01f803afc9e58ac700509c408
Chelong <chelong821204@gmail.com>
2022-08-18 01:09:01 +0800

    fix url execeed card container and add feature for url text content to href link

--- app/frontend/src/components/Card.vue
+++ app/frontend/src/components/Card.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="block flex justify-between border-2 rounded-lg bg-amber-100 my-3 px-4 py-2 border-yellow-900">
-    <div>{{ card.name }}</div>
+    <div class="break-all" v-html="formattedText"></div>
     <i @click="deleteCard(card)">x</i>
   </div>
 </template>
@@ -10,8 +10,21 @@ import { mapActions } from 'vuex'
 export default {
   name: 'CardBlock',
   props: ['card'],
+  computed: {
+    formattedText () {
+      return this.linkify(this.card.name)
+    }
+  },
   methods: {
-    ...mapActions(['deleteCard'])
+    ...mapActions(['deleteCard']),
+    linkify (inputText) {
+      const pattern1 = /(\b(https?|ftp):\/\/[-A-Z0-9+&@#/%?=~_|!:,.;]*[-A-Z0-9+&@#/%=~_|])/gim
+      let text = inputText.replace(pattern1, '<a href="$1" target="_blank">$1</a>')
+
+      const pattern2 = /(^|[^/])(www\.[\S]+(\b|$))/gim
+      text = text.replace(pattern2, '$1<a href="http://$2" target="_blank">$2</a>')
+      return text
+    }
   }
 }
 </script>
